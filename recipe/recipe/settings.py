07428dcc6e84a9e3ff819b30b2a98a1974a3c139
JRGautam123 <jaya.ram@pu.edu.np>
2025-05-26 15:57:46 +0545

    update secret_key length

--- recipe/recipe/settings.py
+++ recipe/recipe/settings.py
@@ -20,8 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='django-insecure-!(fi^cbjfvt=zyk38qj*m8u)*g@2eldm-k+duvp*fr5i3#(!yz'
-
+SECRET_KEY = (
+    'django-insecure-!'
+    '(fi^cbjfvt=zyk38qj*m8u)'
+    '*g@2eldm-k+duvp*fr5i3#(!yz'
+)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
