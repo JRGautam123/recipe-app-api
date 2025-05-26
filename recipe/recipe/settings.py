9f29d4d8c8229993f06ae61c0ff0e02d2ffca708
JRGautam123 <jaya.ram@pu.edu.np>
2025-05-26 15:54:16 +0545

    update intendation

--- recipe/recipe/settings.py
+++ recipe/recipe/settings.py
@@ -20,11 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-!'
-    '(fi^cbjfvt=zyk38qj*m8u)'
-    '*g@2eldm-k+duvp*fr5i3#(!yz'
-    )
+SECRET_KEY ='django-insecure-!(fi^cbjfvt=zyk38qj*m8u)*g@2eldm-k+duvp*fr5i3#(!yz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,9 +87,9 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': (
-        'django.contrib.'
-        'auth.password_validation.'
-        'UserAttributeSimilarityValidator'
+            'django.contrib.'
+            'auth.password_validation.'
+            'UserAttributeSimilarityValidator'
         ),
     },
     {
@@ -105,20 +101,19 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': (
-        'django.contrib.'
-        'auth.password_validation.'
-        'CommonPasswordValidator'
+            'django.contrib.'
+            'auth.password_validation.'
+            'CommonPasswordValidator'
         ),
     },
     {
-    'NAME': (
-        'django.contrib.'
-        'auth.password_validation.'
-        'NumericPasswordValidator'
+        'NAME': (
+            'django.contrib.'
+            'auth.password_validation.'
+            'NumericPasswordValidator'
         ),
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
--- recipe/recipe/urls.py
+++ recipe/recipe/urls.py
@@ -19,5 +19,3 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
-# this is project level urls.py file
