1608053fec39fd4366b343813bf226320f059c5b
JRGautam123 <jaya.ram@pu.edu.np>
2025-06-03 08:10:43 +0545

    build user api, configure all endpoints and test

---
+++ recipe/user/views.py
@@ -0,0 +1,38 @@
+"""Views for User API"""
+
+from rest_framework import generics, authentication, permissions
+from user.serializers import UserSerializer, AuthTokenSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+# class CreateUserView(generics.CreateAPIView):
+#     serializer_class = UserSerializer
+
+
+class CreateUserView(APIView):
+    """View for user api."""
+    def post(self, request, *args, **kwargs):
+        """When user makes post request to the create user api."""
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(UserSerializer(user).data,
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
